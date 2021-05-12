9cf9f6cc3b60a921789e23c82f5457a52e7dcedf
yelgon <yelgon36@gmail.com>
2021-05-11 21:51:51 -0400

    En debut

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h1>Dipika</h1>
+      <h2>Comment ça va?</h2>
+      <h2>Je vais créer votre site Web d'entreprise ici.</h2>
+    </div>
+  );
+}
+
+export default App;
